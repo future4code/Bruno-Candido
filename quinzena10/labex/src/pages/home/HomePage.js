3f5c68da31352567fd6f69c6d02ea92906a1c1d1
Bruno Luan <brunnoluann@live.com>
2021-11-11 13:37:54 -0300

    mudandotudo

--- quinzena10/labex/src/pages/home/HomePage.js
+++ quinzena10/labex/src/pages/home/HomePage.js
@@ -4,24 +4,25 @@ import Footer from '../../components/Footer';
 import Button from '../../components/Button';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import stylled from '../../home/stylled';
 
-const HomePageMainContainer = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0 auto ;
-    background-image:url(https://1.bp.blogspot.com/-Syw0aNZ16tc/XcMdN9_5POI/AAAAAAAAecw/ZlopBKd1iswdN-lIXwdgXAQ7SFcIjBXMACLcBGAsYHQ/s2560/colorful-planets-chill-scifi-pink-4k-zs-2560x1440.jpg)
-`;
+// const HomePageMainContainer = styled.div`
+//     width: 100%;
+//     height: 100vh;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     margin: 0 auto ;
+//     background-image:url(https://1.bp.blogspot.com/-Syw0aNZ16tc/XcMdN9_5POI/AAAAAAAAecw/ZlopBKd1iswdN-lIXwdgXAQ7SFcIjBXMACLcBGAsYHQ/s2560/colorful-planets-chill-scifi-pink-4k-zs-2560x1440.jpg)
+// `;
 
-const ButtonContainer = styled.div`
-    width: 40%;
-    height: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-`;
+// const ButtonContainer = styled.div`
+//     width: 40%;
+//     height: 100%;
+//     display: flex;
+//     justify-content: space-around;
+//     align-items: center;
+// `;
 
 
 function HomePage() {
---
+++ quinzena10/labex/src/pages/home/stylled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const HomePageMainContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto ;
+    background-image:url(https://1.bp.blogspot.com/-Syw0aNZ16tc/XcMdN9_5POI/AAAAAAAAecw/ZlopBKd1iswdN-lIXwdgXAQ7SFcIjBXMACLcBGAsYHQ/s2560/colorful-planets-chill-scifi-pink-4k-zs-2560x1440.jpg)
+`;
+
+const ButtonContainer = styled.div`
+    width: 40%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+export default stylled;
